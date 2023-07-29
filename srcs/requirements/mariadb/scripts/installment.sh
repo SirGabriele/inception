29fbde3a07111c2b5618e0e38e71205a1dd9e598
kbrousse-VM <dsa>
2023-07-29 20:01:07 +0200

    Beginning of mariadb script

--- srcs/requirements/mariadb/scripts/installment.sh
+++ srcs/requirements/mariadb/scripts/installment.sh
@@ -1,2 +1,22 @@
 #! /bin/bash
 
+function info
+{
+	echo -e "\e[0;92m[info] $1\e[0m"
+}
+
+info "Starting mariadb ..."
+service mariadb start
+
+info "Securing databases ..."
+mysql_secure_installation << EOF
+
+y
+y
+password
+password
+y
+y
+y
+y
+EOF
